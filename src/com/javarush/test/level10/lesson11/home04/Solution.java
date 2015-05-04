81f590c604640c7b0b74a92337ed28c3e45b0867
Dimonaz <dmitry.n@pdffiller.com>
2015-05-04 23:03:10 +0300

    Level11

--- src/com/javarush/test/level10/lesson11/home04/Solution.java
+++ src/com/javarush/test/level10/lesson11/home04/Solution.java
@@ -12,13 +12,17 @@ package com.javarush.test.level10.lesson11.home04;
 …
 */
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Solution
 {
     public static void main(String[] args)
     {
         String s = "Я не хочу изучать Java, я хочу большую зарплату";
 
-        //Напишите тут ваш код
+        for(int i = 0; i < 40; i++)
+            System.out.println(s.substring(i));
     }
-
 }
